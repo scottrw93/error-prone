617fcc27121dc7baf622db35beeda023cba5a709
Megan Ketelaar <--global>
2021-01-08 18:33:53 -0500

    Account for backport of Java13 fix to Java 11

--- core/src/main/java/com/google/errorprone/refaster/Inliner.java
+++ core/src/main/java/com/google/errorprone/refaster/Inliner.java
@@ -236,7 +236,9 @@ public final class Inliner {
   private static void setUpperBound(TypeVar typeVar, Type bound) {
     try {
       // https://bugs.openjdk.java.net/browse/JDK-8193367
-      if (RuntimeVersion.isAtLeast13()) {
+      if (RuntimeVersion.isAtLeast11()) {
+        // This method is only in java 11.0.9 or later,
+        // but that should be fine because that's what we use
         TypeVar.class.getMethod("setUpperBound", Type.class).invoke(typeVar, bound);
       } else {
         TypeVar.class.getField("bound").set(typeVar, bound);
